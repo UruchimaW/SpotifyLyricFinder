20c1111ed33e6b95439e26ecfb15b0fdfcb57f58
UruchimaW <uruchimaw@gmail.com>
2020-11-28 11:16:08 -0600

    Credential Verfiication
    No longer need to set environment variables for program to run

--- main.py
+++ main.py
@@ -7,28 +7,29 @@ from spotipy.oauth2 import SpotifyOAuth
 
 scope = "user-library-read, user-read-playback-state"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth( scope=scope,
+                                                client_id=config.SPOTIPY_CLIENT_ID,
+                                                client_secret=config.SPOTIPY_CLIENT_SECRET,
+                                                redirect_uri=config.SPOTIPY_REDIRECT_URI))
 
 info = sp.current_playback()
-# pprint.clea(info)
-pprint.pprint(info["item"]["album"]["artists"][0]["name"])
+# pprint.pprint(info["item"]["name"])
 spotify_artist = info["item"]["album"]["artists"][0]["name"]
-spotify_song = info["item"]["album"]["name"]
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+spotify_song = info["item"]["name"]
 
-# sp.current_playback(market=None, additional_types=None)
+# results = sp.current_user_saved_tracks()
+# for idx, item in enumerate(results['items']):
+#     track = item['track']
+#     print(idx, track['artists'][0]['name'], " – ", track['name'])
 
 genius = lyricsgenius.Genius(config.genius_token)
 user_artist = spotify_artist
 
-artist = genius.search_artist(""+ user_artist +"", max_songs=3, sort="title")
+# artist = genius.search_artist(""+ user_artist +"", max_songs=3, sort="title")
 
 # print(artist.songs)
-song = artist.song(spotify_song)
+# song = artist.song(spotify_song)
 # or:
-# song = genius.search_song("To You", artist.name)
+song = genius.search_song(spotify_song, spotify_artist)
 print(song.lyrics)
 
