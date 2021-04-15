13c2d13d6093783dbfd51ea5bba202ef962bf8ee
UruchimaW <uruchimaw@gmail.com>
2021-04-14 19:56:05 -0500

    Removed dependency on last.fm for images
    Images now come from Spotify API
    Removed PIL from imports as well

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import requests
 import config
 import pprint
 import lyricsgenius
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-
+# from PIL import ImageTk,Image  
 
 scope = "user-library-read, user-read-playback-state"
 
@@ -16,24 +17,17 @@ info = sp.current_playback()
 # playback_state = info["is_playing"]
 # if playback_state == True:
 # print(info)
-pprint.pprint(info)
-spotify_artist = info["item"]["album"]["artists"][0]["name"]
+# pprint.pprint(info["item"]["album"]["images"][0]["url"])
+# spotify_artist = info["item"]["album"]["artists"][0]["name"]
+spotify_artist = info["item"]["artists"][0]["name"]
+spotify_album = info["item"]["album"]["name"]
 spotify_song = info["item"]["name"]
 genius = lyricsgenius.Genius(config.genius_token)
+# print(info["item"]["artists"][0]["name"])
 # user_artist = spotify_artist
 song = genius.search_song(spotify_song, spotify_artist)
+
+album_image = requests.get(info["item"]["album"]["images"][0]["url"])
+with open( spotify_album+".jpg", "wb") as f:
+    f.write(album_image.content)
 print(song.lyrics)
-
-# else:
-#     print("Nothing playing")
-
-# results = sp.current_user_saved_tracks()
-# for idx, item in enumerate(results['items']):
-#     track = item['track']
-#     print(idx, track['artists'][0]['name'], " – ", track['name'])
-
-# artist = genius.search_artist(""+ user_artist +"", max_songs=3, sort="title")
-
-# print(artist.songs)
-# song = artist.song(spotify_song)
-# or:
